af6fdba823e9796f93dafdd44a13807b55d717b4
Michael Morley <mmorley@pennmedicine.upenn.edu>
2019-06-18 16:44:14 -0400

    Allowed for other groups to be used for slingshot

--- R/runSlingshot.R
+++ R/runSlingshot.R
@@ -7,9 +7,12 @@
 #' @import dplyr tidyr Seurat slingshot
 #' @export
 #'
-runSlingshot  <- function(object,reduction='dm',groups=NULL, start.clus=NULL,end.clus=NULL){
+runSlingshot  <- function(object,reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL){
   rd <- Embeddings(object,reduction)
   cl <- Idents(object = object)
+  group.by <- group.by %||% 'ident'
+  cl <- FetchData(object = object, vars = group.by) %>% pull(`group.by`)
+
   object@misc[['sds']] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus))
   ps <- slingPseudotime(object@misc[['sds']]$data)
   object@meta.data[,colnames(ps)] <- ps

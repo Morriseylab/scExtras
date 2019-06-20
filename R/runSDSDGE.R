18e867b9c1d61baa267094dc0457fe14af4d45e9
Michael Morley <mmorley@pennmedicine.upenn.edu>
2019-06-20 12:01:58 -0400

    Renamed the functiona nd fixed vargenes with new assay

--- R/runSDSDGE.R
+++ R/runSDSDGE.R
@@ -1,15 +1,17 @@
 #'run SDSDGE
 #' @param object Seurat object
-#' @import dplyr tidyr Seurat
+#' @import dplyr tidyr Seurat gam
 #' @export
 #'
-runSDSDGE <- function(object){
+runPseudoTimeDGE <- function(object){
+  var <- VariableFeatures(scrna)
+
   DGE <- list()
   for(c in names(object@misc$sds$data@curves)){
-    object@misc$sds$dge[[c]] <- FetchData(object,append(object@var.genes, c,0)) %>% tidyr::gather(gene,signal, -one_of(c)) %>% dplyr::rename(curve = 1) %>%
+    object@misc$sds$dge[[c]] <- FetchData(object,append(var, c,0)) %>% tidyr::gather(gene,signal, -one_of(c)) %>% dplyr::rename(curve = 1) %>%
       tidyr::nest(-gene) %>%
       mutate(
-        fit = purrr::map(data, ~ gam(signal ~ lo(curve), data = .x)),
+        fit = purrr::map(data, ~ gam::gam(signal ~ lo(curve), data = .x)),
         tidied = purrr::map(fit, tidy)
       ) %>%
       tidyr::unnest(tidied) %>%

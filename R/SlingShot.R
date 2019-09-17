af161c319025492b80e24d28cae316c87d02fe4e
Michael Morley <mmorley@pennmedicine.upenn.edu>
2019-09-17 11:11:44 -0400

    Added more cmd line param fro slingshot

--- R/SlingShot.R
+++ R/SlingShot.R
@@ -7,13 +7,13 @@
 #' @import dplyr tidyr Seurat slingshot
 #' @export
 #'
-runSlingshot  <- function(object,reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL, approx_points = FALSE){
+runSlingshot  <- function(object,reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL, approx_points = FALSE, allow.breaks=TRUE){
   rd <- Embeddings(object,reduction)
   cl <- Idents(object = object)
   group.by <- group.by %||% 'ident'
   cl <- FetchData(object = object, vars = group.by) %>% pull(`group.by`)
 
-  object@misc[['sds']] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus,approx_points=approx_points ))
+  object@misc[['sds']] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus,approx_points=approx_points,allow.breaks=allow.breaks ))
   ps <- slingPseudotime(object@misc[['sds']]$data)
   object@meta.data[,colnames(ps)] <- as.data.frame(ps)
   object <- LogSeuratCommand(object = object)

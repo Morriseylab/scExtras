0a7a9948c8baae29cf8bf20527c86f6869d41bb1
bapoorva <bapoorva@mail.med.upenn.edu>
2019-10-10 15:40:39 -0400

    include scTransform

--- R/processExper.R
+++ R/processExper.R
@@ -21,15 +21,16 @@ processExper <- function(dir,name,
                          filter = T,
                          LowerFeatureCutoff=200,
                          UpperFeatureCutoff="MAD",
-                         UpperMitoCutoff=0.05
-                         ){
+                         UpperMitoCutoff=5,
+                         sc.transform=F
+){
   try(if(length(files)==0) stop("No files"))
-
+  
   if(UpperFeatureCutoff!="MAD" & !is.integer(UpperFeatureCutoff)) {
     stop("Please use MAD and numeric cutoff for UpperFeatureCount")
   }
-
-
+  
+  
   if(length(files)==1){
     # Load the dataset
     inputdata <- Read10X(data.dir =files[1])
@@ -45,81 +46,95 @@ processExper <- function(dir,name,
     for(i in 2:length(files)){
       tmp.data <- Read10X(data.dir =files[i])
       colnames(tmp.data) <- paste0(colnames(tmp.data), '-',name, '-rep',i)
-
+      
       tmp.object <- CreateSeuratObject(counts= tmp.data, min.cells = 10, min.features = 200, project = name)
-      # cat('Rep', i, ": ", length(tmp.object@cell.names), "\n", sep="")
+      #cat('Rep', i, ": ", length(tmp.object@cell.names), "\n", sep="")
       object <- merge(object, tmp.object, do.normalize = FALSE, min.cells = 0, min.features = 0)
     }
-    #cat("merged: ", length(object@cell.names), "\n", sep="")
+    # cat("merged: ", length(object@cell.names), "\n", sep="")
   }
-
-
-
+  
   if(org=='mouse'){
-    mito.features <- grep(pattern = "^mt-", x = rownames(x = object), value = TRUE)
+    object[["percent.mito"]] <- PercentageFeatureSet(object, pattern = "^mt-")
   }else{
-    mito.features <- grep(pattern = "^MT-", x = rownames(x = object), value = TRUE)
+    object[["percent.mito"]] <- PercentageFeatureSet(object, pattern = "^MT-")
   }
-
-
-  percent.mito <- Matrix::colSums(x = GetAssayData(object = object, slot = "counts")[mito.features,
-                                                                                     ])/Matrix::colSums(x = GetAssayData(object = object, slot = "counts"))
-
-  # The [[ operator can add columns to object metadata, and is a great place
-  # to stash QC stats
-  object[["percent.mito"]] <- percent.mito
+  
   VlnPlot(object = object, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"),
           ncol = 3)
-
+  
   if(filter){
     #Using a median + 3 MAD cutoff for high genes.
     if(UpperFeatureCutoff=="MAD"){
       UpperFeatureCutoff <- median(object$nFeature_RNA) + 3*mad(object$nFeature_RNA)
     }
-
+    
     object@misc[["filterstats"]] <- list()
     object@misc[["filterstats"]][['TotalSamples']] <- dim(object[[]][1])[1]
-
+    
     cells.use <- colnames(object)[which(object[[]]['percent.mito'] < UpperMitoCutoff)]
     object@misc[["filterstats"]][['Mitofilter']] <- dim(object[[]][1])[1] - length(cells.use)
     object <- subset(object, cells = cells.use)
-
+    
     cells.use <- colnames(object)[which(object[[]]['nFeature_RNA'] > LowerFeatureCutoff)]
     object@misc[["filterstats"]][['LowFeatureFilter']] <- dim(object[[]][1])[1] - length(cells.use)
     object <- subset(object, cells = cells.use)
-
+    
     cells.use <- colnames(object)[which(object[[]]['nFeature_RNA'] < UpperFeatureCutoff)]
     object@misc[["filterstats"]][['HighFeatureFilter']] <- dim(object[[]][1])[1] - length(cells.use)
     object <- subset(object, cells = cells.use)
-
-
+    
+    
   }
-
-  #normalize data
-  object <- NormalizeData(object = object)
-
-  #detection of variable genes
-  #calculates the average expression and dispersion for each gene, places these genes into bins, and then calculates a z-score for dispersion within each bin
-  object <-FindVariableFeatures(object = object,
-                                selection.method = "vst", nfeatures = 2000, verbose = FALSE)
-
-  if(org=='human'){
-    #Assign scores in the CellCycleScoring function.Stores S and G2/M scores in object@meta.data, along with the predicted classification of each cell in either G2M, S or G1 phase
-    object <- CellCycleScoring(object = object, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+  if(sc.transform==T){
+    
+    if(ccscale==T){
+      object <- NormalizeData(object = object)
+      
+      #detection of variable genes
+      #calculates the average expression and dispersion for each gene, places these genes into bins, and then calculates a z-score for dispersion within each bin
+      object <-FindVariableFeatures(object = object,
+                                    selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+      
+      if(org=='human'){
+        #Assign scores in the CellCycleScoring function.Stores S and G2/M scores in object@meta.data, along with the predicted classification of each cell in either G2M, S or G1 phase
+        object <- CellCycleScoring(object = object, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+      }else{
+        m2h <- readr::read_csv(mouseorthologfile)
+        cc.genes$s.genes <- m2h %>% filter(human_name %in% cc.genes$s.genes) %>% pull(mouse_name)
+        cc.genes$g2m.genes <- m2h %>% filter(human_name %in% cc.genes$g2m.genes) %>% pull(mouse_name)
+        object <- CellCycleScoring(object = object, s.features  = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+      }
+      object <- SCTransform(object, verbose = FALSE, vars.to.regress=c("nCount_RNA","percent.mito","S.Score", "G2M.Score"))
+    }else{
+      object <- SCTransform(object, vars.to.regress = c("nCount_RNA","percent.mito"), verbose = FALSE)
+    }
   }else{
-    m2h <- readr::read_csv(mouseorthologfile)
-    cc.genes$s.genes <- m2h %>% filter(human_name %in% cc.genes$s.genes) %>% pull(mouse_name)
-    cc.genes$g2m.genes <- m2h %>% filter(human_name %in% cc.genes$g2m.genes) %>% pull(mouse_name)
-    object <- CellCycleScoring(object = object, s.features  = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+    #normalize data
+    object <- NormalizeData(object = object)
+    
+    #detection of variable genes
+    #calculates the average expression and dispersion for each gene, places these genes into bins, and then calculates a z-score for dispersion within each bin
+    object <-FindVariableFeatures(object = object,
+                                  selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+    
+    if(org=='human'){
+      #Assign scores in the CellCycleScoring function.Stores S and G2/M scores in object@meta.data, along with the predicted classification of each cell in either G2M, S or G1 phase
+      object <- CellCycleScoring(object = object, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+    }else{
+      m2h <- readr::read_csv(mouseorthologfile)
+      cc.genes$s.genes <- m2h %>% filter(human_name %in% cc.genes$s.genes) %>% pull(mouse_name)
+      cc.genes$g2m.genes <- m2h %>% filter(human_name %in% cc.genes$g2m.genes) %>% pull(mouse_name)
+      object <- CellCycleScoring(object = object, s.features  = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+    }
+    
+    if(ccscale==T){
+      #Scaling the data and removing unwanted sources of variation
+      object <- ScaleData(object = object, vars.to.regress = c("nCount_RNA", "percent.mito","S.Score", "G2M.Score"))
+    }else{
+      object <- ScaleData(object = object, vars.to.regress = c("nCount_RNA", "percent.mito"))
+    }
   }
-
-  if(ccscale==T){
-    #Scaling the data and removing unwanted sources of variation
-    object <- ScaleData(object = object, vars.to.regress = c("nCount_RNA", "percent.mito","S.Score", "G2M.Score"))
-  }else{
-    object <- ScaleData(object = object, vars.to.regress = c("nCount_RNA", "percent.mito"))
-  }
-
   object <- LogSeuratCommand(object = object)
   return(object)
 }

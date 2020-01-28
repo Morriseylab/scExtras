e354dd00aa2483424075fe6c2490b350140460c2
Michael Morley <mmorley@pennmedicine.upenn.edu>
2020-01-28 17:00:34 -0500

    Added varaibles extend and stretch to the slignshot function
    Revised the PlotPseudoTime function.

--- R/SlingShot.R
+++ R/SlingShot.R
@@ -4,16 +4,20 @@
 #' @param reduction Reduction method. Default is 'dm'
 #' @param start.clus start cluster
 #' @param end.clus end cluster
+#' @param approx_points
+#' @param allow.breaks logical, determines whether curves that branch very close to the origin should be allowed to have different starting points.
+#' @param extend acter, how to handle root and leaf clusters of lineages when constructing the initial, piece-wise linear curve. Accepted values are 'y' (default), 'n', and 'pc1'. See 'Details' for more.
+#' @param stretch numeric factor by which curves can be extrapolated beyond endpoints. Default is 2, see principal_curve.
 #' @import dplyr tidyr Seurat slingshot
 #' @export
 #'
-runSlingshot  <- function(object,reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL, approx_points = FALSE, allow.breaks=TRUE){
+runSlingshot  <- function(object,reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL, approx_points = FALSE, allow.breaks=TRUE, extend='n',stretch=0){
   rd <- Embeddings(object,reduction)
   cl <- Idents(object = object)
   group.by <- group.by %||% 'ident'
   cl <- FetchData(object = object, vars = group.by) %>% pull(`group.by`)
 
-  object@misc[['sds']] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus,approx_points=approx_points,allow.breaks=allow.breaks ))
+  object@misc[['sds']] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus,approx_points=approx_points,allow.breaks=allow.breaks,extend=extend,stretch=stretch))
   ps <- slingPseudotime(object@misc[['sds']]$data)
   object@meta.data[,colnames(ps)] <- as.data.frame(ps)
   object <- LogSeuratCommand(object = object)
@@ -53,34 +57,44 @@ runPseudoTimeDGE <- function(object){
 #' @export
 #'
 plotPseudoTime = function(object,
+                          sds=NULL,
                           group.by = NULL,
                           reduction = 'dm',
-                          dims = 1:2) {
+                          dims = 1:2
+                          ) {
   object[['ident']] <- Idents(object = object)
   group.by <- group.by %||% 'ident'
   dims <- paste0(Key(object = object[[reduction]]), dims)
 
   curved <-
-    bind_rows(lapply(names(object@misc$sds$data@curves), function(x) {
-      c <- slingCurves(object@misc$sds$data)[[x]]
+    bind_rows(lapply(names(getCurves(sds)), function(x) {
+      c <- slingCurves(sds)[[x]]
       d <- as.data.frame(c$s[c$ord, dims])
       d$curve <- x
       return(d)
     }))
 
 
-  data <- FetchData(object = object, vars = c(dims, group.by))
-  p <- ggplot(data, aes_string(x = dims[1], y = dims[2])) +
-    geom_point(aes(color =!!sym(group.by))) +
-    theme(legend.position = "top")
+  DimPlot(sub,cols=cpallette,label = T,group.by = group.by) +
+    geom_path(aes_string(dims[1], dims[2], linetype = "curve"), curved, size =1)
 
-  if (is.character(data[[group.by]]) | is.factor(data[[group.by]])){
-    p <- p + guides(col = guide_legend(nrow = 2))
-  } else {
-    p <- p + scale_color_distiller(palette = "RdYlBu", na.value = 'grey90')
-  }
-  p <- p +geom_path(aes_string(dims[1], dims[2], linetype = "curve"), curved, size =1)
-  p
+
+
+
+  # data <- FetchData(object = object, vars = c(dims, group.by))
+  # p <- ggplot(data, aes_string(x = dims[1], y = dims[2])) +
+  #   geom_point(aes(color =!!sym(group.by))) +
+  #   theme(legend.position = "top")
+  #
+  # if (is.character(data[[group.by]]) | is.factor(data[[group.by]])){
+  #   p <- p + guides(col = guide_legend(nrow = 2))
+  # } else {
+  #   p <- p + scale_color_distiller(palette = "RdYlBu", na.value = 'grey90')
+  # }
+  # p <- p +geom_path(aes_string(dims[1], dims[2], linetype = "curve"), curved, size =1)
+  # p
+
+
 }
 
 

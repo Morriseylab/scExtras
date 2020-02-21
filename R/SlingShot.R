71f236f6ec00f7fe8c0d0c6b3bd8e15d1204360d
Michael Morley <mmorley@pennmedicine.upenn.edu>
2020-02-21 10:22:12 -0500

    The groupby is not working correct. It will use Idents for now.

--- R/SlingShot.R
+++ R/SlingShot.R
@@ -15,8 +15,9 @@
 runSlingshot  <- function(object,sds.name='sds',reduction='dm',group.by=NULL, start.clus=NULL,end.clus=NULL, approx_points = FALSE, allow.breaks=TRUE, extend='n',stretch=0){
   rd <- Embeddings(object,reduction)
   cl <- Idents(object = object)
-  group.by <- group.by %||% 'ident'
-  cl <- FetchData(object = object, vars = group.by) %>% pull(`group.by`)
+  ### Need to fix this not working
+  #group.by <- group.by %||% 'ident'
+  #cl <- FetchData(object = object, vars = group.by) %>% pull(`group.by`)
 
   object@misc[[sds.name]] <-  list("dr"=reduction,"data"=slingshot(rd,cl,start.clus=start.clus,end.clus=end.clus,approx_points=approx_points,allow.breaks=allow.breaks,extend=extend,stretch=stretch))
   #ps <- slingPseudotime(object@misc[['sds']]$data)

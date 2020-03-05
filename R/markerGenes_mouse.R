f4a5aa2a1c21453f75a92091e75313a4036ab74d
bapoorva <apoorva.babu4@gmail.com>
2020-03-05 11:05:19 -0500

    Add marker genes for mouse and human

---
+++ R/markerGenes_mouse.R
@@ -0,0 +1,3 @@
+markergenes =c("Sox17",	"Vwf","Cxcl12","Fabp4",	"Car4","Prox1","Hopx",	"Ager","Sftpc","Sftpb",
+"Scgb3a2","Foxj1",	"Wnt2","Mfap5","Hhip","Notch3","Pdgfrb","Stc1","Alas2",	"Wt1"	,"Msln",
+"Ptprc",	"Lgals3","Mki67","Pecam1","Pdgfra","Acta2","Ascl2","Scgb1a1")

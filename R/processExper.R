9838ba431fefb53a7d2d85e5ac7bb285039a7cc1
bapoorva <apoorva.babu4@gmail.com>
2022-08-04 14:02:16 -0400

    Remove SCTransform

--- R/processExper.R
+++ R/processExper.R
@@ -1,7 +1,6 @@
-#'Preprocess single cell RNA-seq data with seurat and return seurat object
+#'Preprocess single cell RNA-seq data with seurat and return seurat object. This function only does normalization, finding variable genes and scaling
 #' @param object Seurat object
 #' @param ccscale Specify whether to scale for cell cycle genes or not
-#' @param sc.transform Define whether or not to apply scTransform function in seurat. T/F
 #' @param return_var_genes Define if scale.data matrix should contain only the variable genes. T/F
 #' @param organism Organism (mouse/human)
 #' @return Seurat object
@@ -10,34 +9,10 @@
 #' @examples
 #' scrna = processExper(object=scrna,ccscale=F,sc.transform =F)
 
-processExper <- function(object,ccscale=F,sc.transform=T,return_var_genes = F,org){
+processExper <- function(object,ccscale=F,return_var_genes = F,org){
   data('mouseortholog')
   m2h=mouse_human
-  if(sc.transform==T){
-
-    if(ccscale==T){
-      object <- NormalizeData(object = object)
-
-      #detection of variable genes
-      #calculates the average expression and dispersion for each gene, places these genes into bins, and then calculates a z-score for dispersion within each bin
-      object <-FindVariableFeatures(object = object,
-                                    selection.method = "vst", nfeatures = 2000, verbose = FALSE)
-
-      if(org=='human'){
-        #Assign scores in the CellCycleScoring function.Stores S and G2/M scores in object@meta.data, along with the predicted classification of each cell in either G2M, S or G1 phase
-        object <- CellCycleScoring(object = object, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
-      }else{
-        #m2h <- readr::read_csv(mouseorthologfile)
-        cc.genes$s.genes <- m2h %>% filter(human_name %in% cc.genes$s.genes) %>% pull(mouse_name)
-        cc.genes$g2m.genes <- m2h %>% filter(human_name %in% cc.genes$g2m.genes) %>% pull(mouse_name)
-        object <- CellCycleScoring(object = object, s.features  = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
-      }
-      object <- SCTransform(object, verbose = FALSE, vars.to.regress=c("nCount_RNA","percent.mito","S.Score", "G2M.Score"),return.only.var.genes = return_var_genes)
-    }else{
-      object <- SCTransform(object, vars.to.regress = c("nCount_RNA","percent.mito"),return.only.var.genes = return_var_genes, verbose = FALSE)
-    }
-  }else{
-    #normalize data
+  #normalize data
     object <- NormalizeData(object = object)
 
     #detection of variable genes
@@ -61,6 +36,5 @@ processExper <- function(object,ccscale=F,sc.transform=T,return_var_genes = F,or
     }else{
       object <- ScaleData(object = object, vars.to.regress = c("nCount_RNA", "percent.mito"))
     }
-  }
   object <- LogSeuratCommand(object = object)
 }
